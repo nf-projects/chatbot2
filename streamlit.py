c729b1a6700753fa1c76636c35de348b07594184
nf-projects <nf-projects@users.noreply.github.com>
2022-04-10 11:57:30 -0700

    remove fancy message style

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,13 @@
 from re import L
 import streamlit as st
 # https://github.com/AI-Yash/st-chat
-from streamlit_chat import message
+#from streamlit_chat import message
 from returnreply import returnreply
 
 input = st.text_input('Your Message')
 
 if input:
-    message("You: " + input, is_user=True)
-    message("Jonathan (Bot): " + returnreply(input), is_user=False)
+    #message("You: " + input, is_user=True)
+    st.write(f'You: {input}')
+    st.write(f'Jonathan: {returnreply(input)}')
+    #message("Jonathan (Bot): " + returnreply(input), is_user=False)
