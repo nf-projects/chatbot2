88cbf551337043059689c3e15331652d83d6ead0
nf-projects <nf-projects@users.noreply.github.com>
2022-04-10 11:55:16 -0700

    test

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,7 @@ import streamlit as st
 from streamlit_chat import message
 from returnreply import returnreply
 
-input = st.text_input('Your Message')
+input = st.text_input('Your Message') #awdad
 
 if input:
     message("You: " + input, is_user=True)
